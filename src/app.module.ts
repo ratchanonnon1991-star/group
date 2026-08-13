19f7621817d2a6ccd780e375791c788051c755e4
ratchanonnon1991-star <ratchanonnon.1991@gmail.com>
2026-08-13 18:35:02 +0700

    feat(carts): generate carts resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartsModule } from './carts/carts.module';
 
 @Module({
-  imports: [],
+  imports: [CartsModule],
   controllers: [AppController],
   providers: [AppService],
 })
